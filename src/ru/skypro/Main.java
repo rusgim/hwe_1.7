7431a3fea0430680fb5deb68ded22ab49cb10672
Ruslan <gimazevs@mail.ru>
2022-03-01 20:08:19 +0300

    Задание выполнил, не вижу 4-ое задание иследующие

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,20 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        String fullNameUpp = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpp);
+
+        String fullNameEdit = fullName.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника — " + fullNameEdit);
+
+
     }
+
+
 }
